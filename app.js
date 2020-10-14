5a78c43b4a28bea8a609f699a02beba7b53de6a2
Rachel <rachellsmith.107@gmail.com>
2020-10-14 15:12:30 +0100

    Wednesday afternoon

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const grid = document.querySelector('.grid')
 const ScoreBoard = document.querySelector('.score')
 const livesLeft = document.querySelector('.lives')
 const startButton = document.querySelector('.start')
+const gameEndDisplay = document.querySelector('.gameover')
+const timer = document.querySelector('.timer')
+const highScore = document.querySelector('.highScore')
 
 const width = 9
 const cells = []
@@ -9,12 +12,14 @@ const cells = []
 let score = 0
 let lives = 3
 let frog = 76
+let time = 10
 
 const lilypads = [2, 4, 6]
 const logsRight = [24, 23, 22, 20, 19, 18]
 const logsLeft = [27, 28, 29, 31, 32, 34]
 const yellowCar = [55, 58, 61]
 const policeCar = [65, 68, 71]
+const water = [0, 1, 3, 5, 7, 8, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
 const grass = [9, 10, 11, 12, 13, 14, 15, 16, 17, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 72, 73, 74, 75, 76, 77, 78, 79, 80]
 const roadYellow = [54, 55, 56, 57, 58, 59, 60, 61, 62]
 const roadPolice = [63, 64, 65, 66, 67, 68, 69, 70, 71]
@@ -23,7 +28,9 @@ let moveLogLeftInterval
 let moveLogRightInterval
 let policeCarInterval
 let yellowCarInterval
-let logLeftFrog
+let frogOnLog
+let gainingPoints
+let gameTimer
 
 let startGame = false
 // const endGame = true
@@ -33,10 +40,11 @@ for (let i = 0; i < width ** 2; i++) {
   const div = document.createElement('div')
   div.classList.add('cell')
   grid.appendChild(div)
-  div.innerHTML = i
+  // div.innerHTML = i
   cells.push(div)
 }
 
+//          SETTING UP THE BOARD 
 // ! Starting placement of grid element
 cells[frog].classList.add('frog')
 
@@ -57,9 +65,13 @@ logsLeft.forEach(log => {
   cells[log].classList.add('log')
 })
 
-grass.forEach((grassTile => {
+grass.forEach(grassTile => {
   cells[grassTile].classList.add('grass')
-}))
+})
+
+water.forEach(waterTile => {
+  cells[waterTile].classList.add('water')
+})
 
 roadYellow.forEach(roadTile => {
   cells[roadTile].classList.add('roadYellow')
@@ -70,34 +82,33 @@ roadPolice.forEach(roadTile => {
 })
 
 // ! Moving frog around the board
-function frogMovement() {
-  document.addEventListener('keydown', (event) => {
-    if (!startGame) {
-      return
-    } else {
-      const key = event.key
-      if (key === 'ArrowUp' && !(frog < width)) {
-        cells[frog].classList.remove('frog')
-        frog -= width
-        cells[frog].classList.add('frog')
-      } else if (key === 'ArrowDown' && !(frog > (width ** 2) - width - 1)) {
-        cells[frog].classList.remove('frog')
-        frog += width
-        cells[frog].classList.add('frog')
-      } else if (key === 'ArrowLeft' && !(frog % width === 0)) {
-        cells[frog].classList.remove('frog')
-        frog -= 1
-        cells[frog].classList.add('frog')
-      } else if (key === 'ArrowRight' && !(frog % width === width - 1)) {
-        cells[frog].classList.remove('frog')
-        frog += 1
-        cells[frog].classList.add('frog')
-      }
+// function frogMovement() {
+document.addEventListener('keydown', (event) => {
+  if (!startGame) {
+    return
+  } else {
+    const key = event.key
+    if (key === 'ArrowUp' && !(frog < width)) {
+      cells[frog].classList.remove('frog')
+      frog -= width
+      cells[frog].classList.add('frog')
+    } else if (key === 'ArrowDown' && !(frog > (width ** 2) - width - 1)) {
+      cells[frog].classList.remove('frog')
+      frog += width
+      cells[frog].classList.add('frog')
+    } else if (key === 'ArrowLeft' && !(frog % width === 0)) {
+      cells[frog].classList.remove('frog')
+      frog -= 1
+      cells[frog].classList.add('frog')
+    } else if (key === 'ArrowRight' && !(frog % width === width - 1)) {
+      cells[frog].classList.remove('frog')
+      frog += 1
+      cells[frog].classList.add('frog')
     }
-    gameOver()
-  })
-}
-console.log(startGame)
+  }
+  gameOver()
+})
+// }
 
 // ! Moving yellow car - to the right 
 function carRight() {
@@ -133,7 +144,7 @@ function carLeft() {
   }, 1000)
 }
 
-// ! Moving the logs - to the right - with the frog
+// ! Moving the logs - to the right
 function moveLogRight() {
   moveLogRightInterval = setInterval(() => {
     logsRight.forEach((log, i) => {
@@ -146,22 +157,12 @@ function moveLogRight() {
         logsRight[i] += 1
         cells[log += 1].classList.add('log')
       }
-
-      // }, 600)
-      // setInterval(() => {
-      //   if (frog >= 18 && frog < 26 && cells[frog].classList.contains('log')) {
-
-      //     cells[frog].classList.remove('frog')
-      //     frog += 1
-      //     cells[frog].classList.add('frog')
-      //   }
-
     })
-  }, 599.8)
+  }, 1000)
 }
 
 
-// ! Moving the logs - to the left - with the frog 
+// ! Moving the logs - to the left 
 function moveLogLeft() {
   moveLogLeftInterval = setInterval(() => {
     logsLeft.forEach((log, i) => {
@@ -174,18 +175,23 @@ function moveLogLeft() {
         logsLeft[i] -= 1
         cells[log -= 1].classList.add('log')
       }
-    }, 500)
-    logLeftFrog = setInterval(() => {
-      if (frog >= 27 && frog < 36 && cells[frog].classList.contains('log')) {
-        cells[frog].classList.remove('frog')
-        frog -= 1
-        cells[frog].classList.add('frog')
-      }
     })
-  }, 499.5)
+  }, 1000)
 }
 
-
+function frogLogMove() {
+  frogOnLog = setInterval(() => {
+    if (frog >= 27 && frog < 36 && cells[frog].classList.contains('log')) {
+      cells[frog].classList.remove('frog')
+      frog -= 1
+      cells[frog].classList.add('frog')
+    } else if (frog >= 18 && frog < 26 && cells[frog].classList.contains('log')) {
+      cells[frog].classList.remove('frog')
+      frog += 1
+      cells[frog].classList.add('frog')
+    }
+  }, 999.5)
+}
 
 // Start Button event listener - starts all movement on the page
 startButton.addEventListener('click', () => {
@@ -193,7 +199,16 @@ startButton.addEventListener('click', () => {
     return
   }
   startGame = true
-  frogMovement()
+
+  gameTimer = setInterval(() => {
+    time = time -= 1
+    timer.innerHTML = `Time left ${time}`
+    if (time === 0) {
+      clearInterval(gameTimer)
+    }
+    console.log(time)
+  }, 1000)
+
   carRight()
   carLeft()
   moveLogRight()
@@ -201,9 +216,8 @@ startButton.addEventListener('click', () => {
   frogCrashDetection()
   gameOver()
   frogOnLilypad()
+  frogLogMove()
 })
-
-// console.log(gameRunning)
 
 // Reset the frog to the starting point
 function resetFrog() {
@@ -217,7 +231,9 @@ function resetFrog() {
 function frogCrashDetection() {
   const frogCarCrash = setInterval(() => {
     if (cells[frog].classList.contains('policeCar')
-      || cells[frog].classList.contains('yellowCar')) {
+      || cells[frog].classList.contains('yellowCar')
+      || cells[frog].classList.contains('water') && !cells[frog].classList.contains('log') && !cells[frog].classList.contains('lilypad')) {
+      console.log('frog dead')
       const newLives = lives -= 1
       livesLeft.innerHTML = newLives
       clearInterval(frogCarCrash)
@@ -228,41 +244,47 @@ function frogCrashDetection() {
 
 // Gain points - reaching a lilypad 
 function frogOnLilypad() {
-  lilypads.forEach(lilypad => {
-    if (cells[lilypad].classList.contains('frog')) {
-      // cells[frog].classList.remove('frog')
-      // frog = 76
-      console.log('working')
-      // cells[frog].classList.add('frog')
-      const newScore = score += 1
-      ScoreBoard.innerHTML = newScore
-      // frogOnLilypad()
-    } else {
-      console.log('testing')
+  gainingPoints = setInterval(() => {
+    if (cells[frog].classList.contains('lilypad')) {
+      cells[frog].classList.remove('frog')
+      frog = 76
+      cells[frog].classList.add('frog')
+      const scoring = score += 1
+      ScoreBoard.innerHTML = `${scoring}`
+      frogOnLilypad()
     }
-  })
+  }, 300)
 }
 
-console.log(lilypads)
+function gameDisplay(message, delay) {
+  gameEndDisplay.style.display = 'block'
+  gameEndDisplay.innerHTML = message
+  setTimeout(() => {
+    gameEndDisplay.style.display = 'none'
+  }, delay)
+
+}
+
 function gameOver() {
-  if (lives === 0) {
-    console.log('dead')
+  if (lives === 0 || time === 0) {
     startGame = false
+    gameDisplay(`GAME OVER! <br> You scored ${score} points!`, 3000)
     clearInterval(policeCarInterval)
     clearInterval(yellowCarInterval)
     clearInterval(moveLogLeftInterval)
     clearInterval(moveLogRightInterval)
+    clearInterval(gainingPoints)
+    clearInterval(frogOnLog)
+    clearInterval(gameTimer)
+    document.removeEventListener('keydown', event)
 
     score = 0
     lives = 3
+    time = 10
 
     ScoreBoard.innerHTML = `${score}`
     livesLeft.innerHTML = `${lives}`
     resetFrog()
-
-    // cells[frog].classList.remove('frog')
-    // frog = 76
-    // cells[frog].classList.add('frog')
 
   }
 }
